30204edd5d87ef38a3cbe71daaf8978977101a6b
jwkam <44567894+jwkam@users.noreply.github.com>
2021-05-12 10:30:46 +0900

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,4 +1,4 @@
 # bash -c source /etc/bash.bashrc && jupyter notebook --notebook-dir=/tf --ip 0.0.0.0 --no-browser --allow-root &
 tini -g start-notebook.sh & # jupyter/datascience-notebook:latest start cmd
-python /tf/text_classification.py 
+python /home/jovyan/work/text_classification.py 
 tail -f /dev/null
